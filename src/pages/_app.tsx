43e2da4aa262cbf064992b2b8d20ea4ebdec292b
jdpena28 <johnhenrich1128@gmail.com>
2023-03-01 21:57:45 +0800

    initialize project repo

---
+++ src/pages/_app.tsx
@@ -0,0 +1,46 @@
+import '@/styles/globals.css'
+import type { AppProps } from 'next/app'
+import localFont from 'next/font/local'
+import { Poppins } from 'next/font/google'
+
+
+const brocha = localFont({
+  src: '../../public/fonts/Brocha.woff',
+  variable: '--font-brocha',
+})
+
+const PPNeueMachina = localFont({
+  src: [
+    {
+      path: '../../public/fonts/PPNeueMachina-PlainLight.otf',
+      weight: '100',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/PPNeueMachina-PlainRegular.otf',
+      weight: '300',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/PPNeueMachina-PlainUltrabold.otf',
+      weight: '700',
+      style: 'normal',
+    },
+  ],
+  variable: '--font-ppneuemachina',
+})
+
+const poppins = Poppins({
+  subsets: ['latin'],
+  variable: '--font-poppins',
+  weight: ['100','200','300','400','500','600','700','800','900'],
+  style: ['normal','italic']
+})
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+  <main className={`${brocha.variable} ${PPNeueMachina.variable} ${poppins.variable}`}>
+    <Component {...pageProps} />
+  </main>
+  )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+ 
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ['var(--font-poppins)', 'sans-serif'],
+        neuemachina: ['var(--font-ppneuemachina)', 'sans-serif'],
+        brocha: ['var(--font-brocha)', 'sans-serif'],
+      }
+    },
+  },
+  plugins: [],
+}
