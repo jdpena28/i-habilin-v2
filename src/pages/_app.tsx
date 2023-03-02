4b54733e8ff4b0776e08728b5187ae99402f83f6
jdpena28 <johnhenrich1128@gmail.com>
2023-03-02 11:27:16 +0800

    Add default SEO

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,46 +1,49 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
-import localFont from 'next/font/local'
-import { Poppins } from 'next/font/google'
-
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import localFont from "next/font/local";
+import { Poppins } from "next/font/google";
+import { DefaultSeo } from "next-seo";
+import DEFAULT_SEO from "../../next-seo.config";
 
 const brocha = localFont({
-  src: '../../public/fonts/Brocha.woff',
-  variable: '--font-brocha',
-})
+  src: "../../public/fonts/Brocha.woff",
+  variable: "--font-brocha",
+});
 
 const PPNeueMachina = localFont({
   src: [
     {
-      path: '../../public/fonts/PPNeueMachina-PlainLight.otf',
-      weight: '100',
-      style: 'normal',
+      path: "../../public/fonts/PPNeueMachina-PlainLight.otf",
+      weight: "100",
+      style: "normal",
     },
     {
-      path: '../../public/fonts/PPNeueMachina-PlainRegular.otf',
-      weight: '300',
-      style: 'normal',
+      path: "../../public/fonts/PPNeueMachina-PlainRegular.otf",
+      weight: "300",
+      style: "normal",
     },
     {
-      path: '../../public/fonts/PPNeueMachina-PlainUltrabold.otf',
-      weight: '700',
-      style: 'normal',
+      path: "../../public/fonts/PPNeueMachina-PlainUltrabold.otf",
+      weight: "700",
+      style: "normal",
     },
   ],
-  variable: '--font-ppneuemachina',
-})
+  variable: "--font-ppneuemachina",
+});
 
 const poppins = Poppins({
-  subsets: ['latin'],
-  variable: '--font-poppins',
-  weight: ['100','200','300','400','500','600','700','800','900'],
-  style: ['normal','italic']
-})
+  subsets: ["latin"],
+  variable: "--font-poppins",
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  style: ["normal", "italic"],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-  <main className={`${brocha.variable} ${PPNeueMachina.variable} ${poppins.variable}`}>
-    <Component {...pageProps} />
-  </main>
-  )
+    <main
+      className={`${brocha.variable} ${PPNeueMachina.variable} ${poppins.variable}`}>
+      <DefaultSeo {...DEFAULT_SEO} />
+      <Component {...pageProps} />
+    </main>
+  );
 }
