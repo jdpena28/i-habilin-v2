b2f18b4257fd7d4acfa911f6d958abe0b4bdb6f2
jdpena28 <johnhenrich1128@gmail.com>
2023-03-02 11:33:12 +0800

    Add custom color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,18 +4,24 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
- 
+
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
+      colors: {
+        primary: "#FB752A",
+        secondary: "#FFCB45",
+        tertiary: "#F1F1EA",
+        highlight: "#101110",
+      },
       fontFamily: {
-        poppins: ['var(--font-poppins)', 'sans-serif'],
-        neuemachina: ['var(--font-ppneuemachina)', 'sans-serif'],
-        brocha: ['var(--font-brocha)', 'sans-serif'],
-      }
+        poppins: ["var(--font-poppins)", "sans-serif"],
+        neuemachina: ["var(--font-ppneuemachina)", "sans-serif"],
+        brocha: ["var(--font-brocha)", "sans-serif"],
+      },
     },
   },
   plugins: [],
-}
+};
