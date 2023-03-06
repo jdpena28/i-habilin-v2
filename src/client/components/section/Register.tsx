ef3bde2aea903006358b07600fad5f181080a44c
rtroy91 <rtbocasiones@bpsu.edu.ph>
2023-03-06 23:21:21 +0800

    Add Register Section

--- src/client/components/section/Register.tsx
+++ src/client/components/section/Register.tsx
@@ -1,5 +1,28 @@
+import RegisterOrdering from "../../../../public/receipt-ordering";
+import StatisticalDashboard from "../../../../public/statistical-dashboard";
+
 const Register = () => {
-  return <section>Register</section>;
+  return (
+    <section className="flex h-screen flex-col-reverse items-center justify-center md:flex-row">
+      <div className="flex flex-col items-center space-y-2 pl-0 text-center md:items-start md:pl-12 md:text-left">
+        <h3 className="text-primary">Enjoy all the great features</h3>
+        <p className=" w-4/5 pb-4">
+          A revolutionized and modernized ordering system right away to your
+          smartphone. You can now apply for the system and enjoy all the great
+          features of the I-Habilin.
+        </p>
+        <button
+          type="button"
+          className="focus:tertiary bg-secondary text-highlight hover:bg-primary focus:ring">
+          Apply Now
+        </button>
+      </div>
+      <div className="relative mx-10 pr-14">
+        <RegisterOrdering />
+        <StatisticalDashboard />
+      </div>
+    </section>
+  );
 };
 
 export default Register;
