61bed41fc8c5f0d9746b3777f792c0f8b955bf4f
jdpena28 <johnhenrich1128@gmail.com>
2023-03-07 14:14:49 +0800

    Add jumbotron section

--- src/client/components/section/Jumbotron.tsx
+++ src/client/components/section/Jumbotron.tsx
@@ -1,6 +1,26 @@
+import GirlOrderingFood from "../../../../public/girl-ordering-food";
+
 const Jumbotron = () => {
   return (
-    <section className="flex items-center justify-between">Jumbotron</section>
+    <section className="flex h-screen flex-col items-center justify-between md:flex-row">
+      <div className="space-y-5">
+        <div className="decorated-underline">
+          <h1>I-Habilin</h1>
+          <div />
+        </div>
+        <p className="subheading max-w-xl">
+          A revolutionize ordering system. Lorem ipsum dolor sit amet
+          consectetur. Non nulla eget eu pharetra at. Dignishsim tortor diam
+          ullamcorper eget mi.{" "}
+        </p>
+        <button type="button" className="bg-primary text-white">
+          See Features
+        </button>
+      </div>
+      <div>
+        <GirlOrderingFood />
+      </div>
+    </section>
   );
 };
 
