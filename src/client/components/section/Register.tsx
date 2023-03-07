8ec1aa782d5b8a91de52d1f58862bc5f519bf6a7
rtroy91 <rtbocasiones@bpsu.edu.ph>
2023-03-07 21:54:46 +0800

    Fix Register Section

--- src/client/components/section/Register.tsx
+++ src/client/components/section/Register.tsx
@@ -1,9 +1,10 @@
+import Link from "next/link";
 import RegisterOrdering from "../../../../public/receipt-ordering";
 import StatisticalDashboard from "../../../../public/statistical-dashboard";
 
 const Register = () => {
   return (
-    <section className="flex h-screen flex-col-reverse items-center justify-center md:flex-row">
+    <section className="flex min-h-screen flex-col-reverse items-center justify-center md:flex-row">
       <div className="flex flex-col items-center space-y-2 pl-0 text-center md:items-start md:pl-12 md:text-left">
         <h3 className="text-primary">Enjoy all the great features</h3>
         <p className=" w-4/5 pb-4">
@@ -11,15 +12,19 @@ const Register = () => {
           smartphone. You can now apply for the system and enjoy all the great
           features of the I-Habilin.
         </p>
-        <button
-          type="button"
-          className="focus:tertiary bg-secondary text-highlight hover:bg-primary focus:ring">
-          Apply Now
-        </button>
+        <Link href="/be-a-stall-owner">
+          <button
+            type="button"
+            className="focus:tertiary bg-secondary text-highlight hover:bg-primary focus:ring">
+            Apply Now
+          </button>
+        </Link>
       </div>
       <div className="relative mx-10 pr-14">
         <RegisterOrdering />
-        <StatisticalDashboard />
+        <div className="absolute -top-[4.3rem] right-0">
+          <StatisticalDashboard />
+        </div>
       </div>
     </section>
   );
