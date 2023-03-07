832b5621883f3bd1929955df0076615bb496e44d
jdpena28 <johnhenrich1128@gmail.com>
2023-03-07 22:48:02 +0800

    FIX: class styling

--- src/client/components/section/Register.tsx
+++ src/client/components/section/Register.tsx
@@ -5,9 +5,9 @@ import StatisticalDashboard from "../../../../public/statistical-dashboard";
 const Register = () => {
   return (
     <section className="flex min-h-screen flex-col-reverse items-center justify-center md:flex-row">
-      <div className="flex flex-col items-center space-y-2 pl-0 text-center md:items-start md:pl-12 md:text-left">
+      <div className="flex flex-col items-center gap-y-5  text-center md:items-start  md:text-left">
         <h3 className="text-primary">Enjoy all the great features</h3>
-        <p className=" w-4/5 pb-4">
+        <p className=" w-4/5">
           A revolutionized and modernized ordering system right away to your
           smartphone. You can now apply for the system and enjoy all the great
           features of the I-Habilin.
@@ -15,7 +15,7 @@ const Register = () => {
         <Link href="/be-a-stall-owner">
           <button
             type="button"
-            className="focus:tertiary bg-secondary text-highlight hover:bg-primary focus:ring">
+            className="focus:tertiary bg-secondary text-black hover:bg-primary focus:ring">
             Apply Now
           </button>
         </Link>
