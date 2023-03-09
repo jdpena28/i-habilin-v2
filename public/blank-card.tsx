dec409caf218b6f7a2bd98964415551832882670
unknown <Perdiojef330@gmail.com>
2023-03-09 14:09:04 +0800

    Add feature page

---
+++ public/blank-card.tsx
@@ -0,0 +1,9 @@
+const BlankCard = () =>{
+    return (
+        <svg className="h-auto w-full"  viewBox="0 0 434 300" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M0 20C0 8.9543 8.95431 0 20 0H414C425.046 0 434 8.95431 434 20V280C434 291.046 425.046 300 414 300H20C8.95432 300 0 291.046 0 280V20Z" fill="#E8DFC8"/>
+</svg>
+    )
+}
+
+export default BlankCard
--- src/client/components/section/Features.tsx
+++ src/client/components/section/Features.tsx
@@ -1,14 +1,42 @@
+import BlankCard from "@public/blank-card";
+import BlankICard from "@public/BlankI-Card";
+import DashboardCard from "@public/Dashboard-card";
+import FoodPreparation from "@public/Food-Preparation";
+import IotCard from "@public/IoT-Card";
+import MaintenanceCard from "@public/Maintenance-card";
+import TechologyCard from "@public/Technology-Card";
+
 const Features = () => {
   return (
-    <section>
-      <div className="grid grid-flow-col grid-rows-3 gap-4">
-        <img className="row-start-1" src="Card-container.jpg" alt="" />
-        <img className="col-span-2" src="Card-container.jpg" alt="" />
-        <img
-          className="col-span-2 row-span-2"
-          src="Card-container.jpg"
-          alt=""
-        />
+    <section className="m-10">
+      <div className=" gap-5 md:columns-3">
+        <div className="mb-6 aspect-video w-auto">
+          <BlankCard />
+        </div>
+        <div className="mb-6 aspect-square w-auto">
+          <DashboardCard />
+        </div>
+        <div className="mb-6 aspect-video w-auto">
+          <BlankICard />
+        </div>
+        <div className="mb-6 aspect-square w-auto">
+          <MaintenanceCard />
+        </div>
+        <div className="mb-8 aspect-square w-auto">
+          <TechologyCard />
+        </div>
+        <div className="mb-6 aspect-video w-auto">
+          <BlankCard />
+        </div>
+        <div className="mb-6 aspect-square w-auto">
+          <IotCard />
+        </div>
+        <div className="mb-6 aspect-video w-auto">
+          <BlankCard />
+        </div>
+        <div className="mb-6 aspect-square w-auto">
+          <FoodPreparation />
+        </div>
       </div>
     </section>
   );
