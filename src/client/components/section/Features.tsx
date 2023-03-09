60dccf0613203c2e30f45bbcd61a394191f5c7e0
jdpena28 <johnhenrich1128@gmail.com>
2023-03-09 23:07:04 +0800

    REFACTOR: feature section

--- src/client/components/section/Features.tsx
+++ src/client/components/section/Features.tsx
@@ -1,41 +1,60 @@
-import BlankCard from "@public/blank-card";
-import BlankICard from "@public/BlankI-Card";
-import DashboardCard from "@public/Dashboard-card";
-import FoodPreparation from "@public/Food-Preparation";
-import IotCard from "@public/IoT-Card";
-import MaintenanceCard from "@public/Maintenance-card";
-import TechologyCard from "@public/Technology-Card";
+import FeatureDashboard from "@public/public/feature-dashboard";
+import FeatureIOT from "@public/public/feature-iot";
+import FeatureMaintenance from "@public/public/feature-maintenance";
+import FeaturePreparation from "@public/public/feature-preparation";
+import Image from "next/image";
 
 const Features = () => {
   return (
-    <section className="m-10">
-      <div className=" gap-5 md:columns-3">
-        <div className="mb-6 aspect-video w-auto">
-          <BlankCard />
+    <section id="feature" className="min-h-screen w-full lg:h-[120vh]">
+      <div className="grid h-full grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3">
+        <div className="flex flex-col gap-5">
+          <div className="hidden h-full w-full flex-[32.5] rounded-md bg-[#E8DFC8] lg:block" />
+          <div className="feature-public-card">
+            <FeatureDashboard />
+            <p className="heading text-2xl">Dashboard</p>
+            <p className="subheading">
+              Displays a comprehensive representation of data from day-to-day
+              transactions.
+            </p>
+          </div>
+          <div className="hidden h-full w-full flex-[32.5] rounded-md bg-[#E8DFC8] lg:block" />
         </div>
-        <div className="mb-6 aspect-square w-auto">
-          <DashboardCard />
+        <div className="flex flex-col justify-center gap-5">
+          <div className="feature-public-card">
+            <FeatureMaintenance />
+            <p className="heading text-2xl">Menu Maintenance</p>
+            <p className="subheading">
+              Customization of the food menu made it easy and hassle-free.
+            </p>
+          </div>
+          <div className="feature-public-card">
+            <div className="relative h-full max-h-[75%] w-full">
+              <Image src="/public/feature-qr1.svg" alt="QR Code Icon" fill />
+            </div>
+            <p className="heading text-2xl">QR Code Technology</p>
+            <p className="subheading">
+              Obtaining food menu information via QR code
+            </p>
+          </div>
         </div>
-        <div className="mb-6 aspect-video w-auto">
-          <BlankICard />
-        </div>
-        <div className="mb-6 aspect-square w-auto">
-          <MaintenanceCard />
-        </div>
-        <div className="mb-8 aspect-square w-auto">
-          <TechologyCard />
-        </div>
-        <div className="mb-6 aspect-video w-auto">
-          <BlankCard />
-        </div>
-        <div className="mb-6 aspect-square w-auto">
-          <IotCard />
-        </div>
-        <div className="mb-6 aspect-video w-auto">
-          <BlankCard />
-        </div>
-        <div className="mb-6 aspect-square w-auto">
-          <FoodPreparation />
+        <div className="flex flex-col gap-5">
+          <div className="feature-public-card">
+            <FeatureIOT />
+            <p className="heading text-2xl">IoT Technology</p>
+            <p className="subheading">
+              Notify customers using vibration through a smartphone.
+            </p>
+          </div>
+          <div className="feature-public-card">
+            <FeaturePreparation />
+            <p className="heading text-2xl">Food Preparation</p>
+            <p className="subheading">
+              Ensuring customer satisfaction and a positive dining experience
+              through efficient preparation.
+            </p>
+          </div>
+          <div className="hidden h-32 w-full flex-[.30] rounded-md bg-[#E8DFC8] lg:block" />
         </div>
       </div>
     </section>
