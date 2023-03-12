2c64290ef53c445152c7870b6d01cfcd4c530838
jdpena28 <johnhenrich1128@gmail.com>
2023-03-12 22:21:48 +0800

    FIX: property svg for jsx format

--- public/background-image.tsx
+++ public/background-image.tsx
@@ -1,7 +1,7 @@
 const BackgroundImage = () => {
   return (
     <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
-      width="100%" viewBox="0 0 440 693" enable-background="new 0 0 557 693">
+      width="100%" viewBox="0 0 440 693" enableBackground="new 0 0 557 693">
       <path fill="#FEF5CB" opacity="1.000000" stroke="none"
         d="
 M558.000000,321.000000 
