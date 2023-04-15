8e97ed6e93c69b8da77fb2ac4d66eecabd5cf554
jdpena28 <johnhenrich1128@gmail.com>
2023-04-15 15:35:28 +0800

    Add users page and adding of users on stalls

--- src/server/routers/stall/index.ts
+++ src/server/routers/stall/index.ts
@@ -1,6 +1,8 @@
 import { router } from "@/server/trpc";
 import { categoryRouter } from "./category";
+import { userRouter } from "./user";
 
 export const stallRouter = router({
   menu: categoryRouter,
+  user: userRouter,
 });
