63ab879b0823c4146b201b94bd9e224f9819c2e8
jdpena28 <johnhenrich1128@gmail.com>
2023-04-19 13:44:57 +0800

    Add maintenance page

--- src/pages/maintenance.tsx
+++ src/pages/maintenance.tsx
@@ -2,23 +2,12 @@ import ErrorMsg from "@public/error-msg";
 
 const Maintenance = () => {
   return (
-    <section className="bg-white pt-28">
-      <p className="mx-5 pt-10 text-center font-brocha text-5xl md:hidden ">
-        ERROR 404 !
-      </p>
-      <div className=" pt-00 flex justify-center">
+    <section className="flex h-screen w-full items-center justify-center">
+      <div className="container mx-auto space-y-5 px-2">
         <ErrorMsg />
-      </div>
-      <p className="invisible mx-5 text-center font-brocha text-[70px] md:visible ">
-        ERROR 404 !
-      </p>
-      <div className="texts-center flex justify-center p-6 text-center text-2xl">
-        <p>Looks like this page is currently down for Maintenance</p>
-      </div>
-      <div className="flex items-center justify-center">
-        <button className=" focus:shadow-outline-blue  rounded-lg border border-transparent bg-secondary px-4 py-2 text-sm font-medium leading-5 text-white shadow transition-colors duration-150 hover:bg-primary focus:outline-none">
-          Back to homepage
-        </button>
+        <p className="heading text-center text-xl lg:text-3xl">
+          Looks like this page is currently down for Maintenance
+        </p>
       </div>
     </section>
   );
